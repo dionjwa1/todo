f93ec04e987de0ed197a072358d070a03d9ac3b8
dionjwa1 <dionjwa@gmail.com>
2021-06-28 19:12:05 -0700

    lab11

--- caps/caps/caps.js
+++ caps/caps/caps.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const vendors = require('../models/vendors.js');
 const events = require('./events/events.js');
 const handleDrivers = require('./models/drivers.js');
 const handleVendors = require('./models/vendors.js');
@@ -7,3 +8,9 @@ const handleVendors = require('./models/vendors.js');
 events.on('sweat', handleDrivers);
 events.on('sunglasses', handleVendors);
 
+const randomCustomer = new vendors.Vendor();
+
+setInterval(() => {
+  
+    events.emit('order', { order: randomCustomer });
+  }, 5000);
